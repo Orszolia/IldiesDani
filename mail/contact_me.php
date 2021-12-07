872c38b961c2b29c1b6eaeb01fe596f36f56b785
Orszolia <vermesorsi@gmail.com>
2021-12-07 19:39:45 +0100

    Orsi v1 javítások

--- mail/contact_me.php
+++ mail/contact_me.php
@@ -1,26 +1,28 @@
 <?php
 // Check for empty fields
-if(empty($_POST['name'])      ||
-   empty($_POST['email'])     ||
-   empty($_POST['phone'])     ||
-   empty($_POST['message'])   ||
-   !filter_var($_POST['email'],FILTER_VALIDATE_EMAIL))
-   {
-   echo "No arguments Provided!";
-   return false;
-   }
-   
+if(empty($_POST['name']) || empty($_POST['email']) || empty($_POST['phone']) || !filter_var($_POST['email'], FILTER_VALIDATE_EMAIL)) {
+  http_response_code(500);
+  exit();
+}
+
 $name = strip_tags(htmlspecialchars($_POST['name']));
-$email_address = strip_tags(htmlspecialchars($_POST['email']));
+$plus_names = $_POST['plus_names'];
+$plus_names_imp = implode(", ", $plus_names);
+$email = strip_tags(htmlspecialchars($_POST['email']));
 $phone = strip_tags(htmlspecialchars($_POST['phone']));
 $message = strip_tags(htmlspecialchars($_POST['message']));
-   
+$coming = strip_tags(htmlspecialchars($_POST['coming']));
+$room = strip_tags(htmlspecialchars($_POST['room']));
+$room_for = strip_tags(htmlspecialchars($_POST['room_for']));
+
 // Create the email and send the message
-$to = 'info@aq-solutions.hu'; // Add your email address inbetween the '' replacing yourname@yourdomain.com - This is where the form will send a message to.
-$email_subject = "$name fel szeretné venni veled a kapcsolatot";
-$email_body = "Az aq solutions weboldalról érkezett egy megkeresés.\n\n"."A részletek:\n\nNév: $name\n\nEmail: $email_address\n\nTelefonszám: $phone\n\nAz üzenete:\n$message";
-$headers = "From: noreply@aq-solutions.hu\n"; // This is the email address the generated message will be from. We recommend using something like noreply@yourdomain.com.
-$headers .= "Reply-To: $email_address";   
-mail($to,$email_subject,$email_body,$headers);
-return true;         
+$to = "z.ildiko.93@gmail.com"; // Add your email address in between the "" replacing yourname@yourdomain.com - This is where the form will send a message to.
+$subject = " $name válasza: $coming";
+$body = "Visszajelzés érkezett az esküvői meghívóra:\n\n".
+"Név: $name\n\nKísérői: $plus_names_imp\n\nEmail: $email\n\nTelefonszám: $phone\n\nVálasza: $coming\n\nSzállás és shuttle igény: $room $room_for fő számára\n\nÜzenete:\n$message";
+$header = "From: noreply@fruzsizoli.hu\n"; // This is the email address the generated message will be from. We recommend using something like noreply@yourdomain.com.
+$header .= "Reply-To: $email";	
+
+if(!mail($to, $subject, $body, $header))
+  http_response_code(500);
 ?>
